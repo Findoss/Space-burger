6e4f215b5948922c627f711bc1f6a412e4bf8b0e
Nikita Stroganov <findoss@yandex.ru>
2022-01-19 22:58:02 +0300

    add debug

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import { App } from './app/App';
 // import reportWebVitals from './plugins/webVitals/reportWebVitals';
 
+console.log(process.env.REACT_APP_VERSION);
+console.log(process.env.REACT_APP_PUBLIC_URL);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
