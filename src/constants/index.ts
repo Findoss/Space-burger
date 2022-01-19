7c9a425167969c4cd19cd9c639c38f29555c6fb3
Nikita Stroganov <findoss@yandex.ru>
2022-01-19 23:04:53 +0300

    fix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,10 @@
 export const IS_DEV = process.env.NODE_ENV === 'development';
+export const IS_PROD = process.env.NODE_ENV === 'production';
 
 export const PROTOCOL = 'https://';
 export const HOST = 'norma.nomoreparties.space';
 export const PORT = '';
+export const PREFIX = IS_PROD ? process.env.REACT_APP_PUBLIC_URL : '';
 
 export const API_VERSION = 'api';
-export const API_URL = `${PROTOCOL}${HOST}${PORT}/${API_VERSION}`;
+export const API_URL = `${PROTOCOL}${HOST}${PORT}/${API_VERSION}${PREFIX}`;
