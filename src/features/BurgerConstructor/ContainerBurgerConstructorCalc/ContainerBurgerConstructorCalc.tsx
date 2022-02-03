1e3211b81e002cc8c63ece0ec7aab07755b3faa9
Nikita Stroganov <nstroganov@adsterra.com>
2022-02-03 11:57:49 +0300

    add modal

--- src/features/BurgerConstructor/ContainerBurgerConstructorCalc/ContainerBurgerConstructorCalc.tsx
+++ src/features/BurgerConstructor/ContainerBurgerConstructorCalc/ContainerBurgerConstructorCalc.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import cn from 'classnames';
-import { useSelector } from 'hooks/useRedux';
+import { useSelector, useDispatch } from 'hooks/useRedux';
 
 import { BurgerConstructorCalc } from 'components/BurgerConstructorCalc';
 
 import { selectSumOrder } from '../service/selectors';
+import { toggleModalOrder } from '../service/slice';
 
 import styles from './styles.module.css';
 
@@ -12,14 +13,20 @@ import type { Props } from './types';
 import { useTranslation } from 'react-i18next';
 
 export const ContainerBurgerConstructorCalc = ({ extraClass }: Props) => {
+  const dispatch = useDispatch();
   const sumOrder = useSelector(selectSumOrder) || 0;
   const { t } = useTranslation();
+
+  const handlerClick = () => {
+    dispatch(toggleModalOrder());
+  };
 
   return (
     <BurgerConstructorCalc
       extraClass={extraClass}
       sumOrder={sumOrder}
       textButton={t('constructor.buy')}
+      onClick={handlerClick}
     />
   );
 };
