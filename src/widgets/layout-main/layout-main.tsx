511018a3cbf1f9ec4bd23362ba145b3d1df032b7
Nikita Stroganov <findoss@yandex.ru>
2022-04-30 11:29:21 +0300

    move app header

--- src/widgets/layout-main/layout-main.tsx
+++ src/widgets/layout-main/layout-main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { Props } from './types';
 
-import { AppHeader } from 'features/app-header/app-header';
+import { AppHeader } from 'widgets/app-header/app-header';
 
 export const Main = ({ children }: Props) => {
   return (
